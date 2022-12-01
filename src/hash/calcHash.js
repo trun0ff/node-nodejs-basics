f21b307f6c25033ba48b78a6f3f41b7722948a6b
trun0ff <freeze2991@gmail.com>
2022-12-01 15:30:21 +0200

    feat: complete hash section

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { promises as fs } from "fs";
+import crypto from "crypto";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = __dirname + '/files/fileToCalculateHashFor.txt'
+    fs.readFile(filePath, 'utf8')
+        .then((res) => {
+            const hashSum = crypto.createHash('sha256')
+            hashSum.update(res)
+            return hashSum.digest('hex')
+        })
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.error(err.message)
+        })
 };
 
 await calculateHash();
