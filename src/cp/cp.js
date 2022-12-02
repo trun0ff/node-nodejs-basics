18376f5705af258dd2174ca0eb5beebc4506254c
trun0ff <freeze2991@gmail.com>
2022-12-02 19:41:01 +0200

    feat: complete child process section

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import child_process from "child_process"
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import path from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+        child_process.fork(
+            path.resolve(__dirname, 'files/script.js'),
+            args,
+            { stdio: 'inherit' }
+        )
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv.slice(2));
