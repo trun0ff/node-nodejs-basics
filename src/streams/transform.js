f5862193a0eaed665bda81b095c726bda12f1a63
trun0ff <freeze2991@gmail.com>
2022-12-02 13:17:31 +0200

    fix: transform stream import

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,12 +1,13 @@
 import { stdin as input, stdout as output } from "process"
-import Transform from "stream"
+import { Transform } from "stream"
 
 const transform = async () => {
 
     const textRevertStream = new Transform({
         transform(chunk, encoding, callback) {
-            const resultString = chunk.toString().split("").reverse().join("")
+            const resultString = chunk.toString().split("").reverse().join("").trim()
             this.push(resultString)
+            this.push('\n')
             callback();
         },
     });
