4ac4767bc688c17c4c48c06227aeb274ddd2b2f8
trun0ff <freeze2991@gmail.com>
2022-12-02 18:41:05 +0200

    feat: complete workers section

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,49 @@
+import {Worker} from "worker_threads"
+import os from "os"
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const cpuCoresCount = os.cpus().length,
+    __dirname = dirname(fileURLToPath(import.meta.url)),
+    workerFilePath = __dirname + '/worker.js',
+    startValue = 10
+
+const createWorkerPromise = (counter) => {
+    return new Promise((resolve, reject) => {
+        let w =  new Worker(workerFilePath)
+        w.on('message', (msg) => {
+            return resolve(msg)
+        })
+        w.on('error', (err) => {
+            return reject(err)
+        })
+        w.postMessage(startValue + counter)
+    });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    let workerPromises = [],
+        counter = 0,
+        workersResults = []
+
+    while(counter < cpuCoresCount) {
+        workerPromises.push(createWorkerPromise(counter))
+        counter++
+    }
+
+    Promise.allSettled(workerPromises)
+        .then((results) => {
+            results.forEach((result) => {
+                workersResults.push({
+                    status: (result.status === 'fulfilled' ? 'resolved' : 'error'),
+                    data: (result.status === 'fulfilled' ? result.value : null),
+                })
+            })
+            console.log(workersResults)
+        })
+        .catch(function(err) {
+            console.error(err.message);
+        });
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,18 @@
+import {parentPort} from "worker_threads"
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const sendResult = (msg) => {
+    // console.log(`finish worker with ${msg}`)
+    parentPort.postMessage(msg)
 };
 
-sendResult();
+parentPort.on('message', msg => {
+    try{
+        sendResult(nthFibonacci(parseInt(msg)));
+    } catch (e) {
+        throw new Error('Error from worker')
+    }
+
+});
